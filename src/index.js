89ec1c89ce5b1ab90a187d306d828e14f95ceb47
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2017-04-29 22:00:50 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Diory () {
+  return (
+    <div>Hello, I am a Diory!</div>
+  )
+}
---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+/* eslint-env jest */
+import React from 'react'
+import { shallow } from 'enzyme'
+import Diory from './index'
+
+describe('<Diory />', () => {
+  it('renders', () => {
+    const wrapper = shallow(<Diory />)
+    expect(wrapper.html()).toEqual('<div>Hello, I am a Diory!</div>')
+  })
+})
